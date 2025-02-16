371f4fc510ce6cdd03edbfadb20f354b51d1e7c2
torodriguezt <torodriguezt@unal.edu.co>
2025-02-16 13:23:47 -0500

    CORS fastAPI

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 import numpy as np
 from PIL import Image
 import io
 from tensorflow.keras.models import load_model
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 MODEL_PATH = "preentrenado.keras"
 model = load_model(MODEL_PATH)
